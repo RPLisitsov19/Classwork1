11f71e9684c8201ea14f94917291f47650054fc2
Радослав Пламенов Лисицов <RPLisitsov19@codingburgas.bg>
2023-09-25 12:11:22 +0300

    Presentation 2

---
+++ Project2/src/BankAccount.java
@@ -0,0 +1,12 @@
+public class BankAccount {
+    String id;
+    double balance;
+
+    public String getId() {
+        return id;
+    }
+    public String setId(String id){
+        this.id = id;
+
+    }
+}
---
+++ Project2/src/Dice.java
@@ -0,0 +1,23 @@
+public class Dice {
+    private int sides;
+
+    String type;
+    int[] rollFrequency;
+    Person owner;
+
+    public void roll(int amount) {
+
+    }
+
+    public int Sides
+    {
+        public int getSides(){
+            return this.sides;
+    }
+    public int setSides() {
+            this.sides = value;
+    }
+
+    }
+
+}
